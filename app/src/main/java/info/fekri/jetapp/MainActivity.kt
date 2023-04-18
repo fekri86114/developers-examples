1a62f3c74590019d26932af344b6137bd7f245a2
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-04-18 10:50:20 -0700

    `lemon tree` is ready

--- app/src/main/java/info/fekri/jetapp/MainActivity.kt
+++ app/src/main/java/info/fekri/jetapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
@@ -13,6 +14,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,49 +28,130 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetAppTheme {
-                DiceRollerApp()
+                LemonApp()
             }
         }
     }
 }
 
-@Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(align = Alignment.Center)
-    )
+fun LemonApp() {
+    var currentStep by remember { mutableStateOf(1) }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        when (currentStep) {
+
+            1 -> {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.select_lemon),
+                        fontFamily = FontFamily.Cursive,
+                        fontSize = 30.sp,
+                        color = Color.Green
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.lemon_tree),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 2
+                            }
+                    )
+
+                }
+            }
+
+            2 -> {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.lemon_squeeze),
+                        fontFamily = FontFamily.Cursive,
+                        fontSize = 30.sp,
+                        color = Color.Green
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.lemon_squeeze),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 3
+                            }
+                    )
+                }
+            }
+
+            3 -> {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.tap_drink),
+                        fontFamily = FontFamily.Cursive,
+                        fontSize = 30.sp,
+                        color = Color.Green
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.lemon_drink),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 4
+                            }
+                    )
+                }
+            }
+
+            4 -> {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.restart_message),
+                        fontFamily = FontFamily.Cursive,
+                        fontSize = 30.sp,
+                        color = Color.Green
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.lemon_restart),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .wrapContentSize()
+                            .clickable {
+                                currentStep = 1
+                            }
+                    )
+                }
+            }
+
+        }
+    }
+
 }
 
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun DiceWithButtonAndImage(modifier : Modifier = Modifier) {
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when (result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
-
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Image(
-            painter = painterResource(id = imageResource) ,
-            contentDescription = result.toString()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = { result = (1..6).random() }) {
-            Text(stringResource(id = R.string.roll))
-        }
-
-    }
+fun DefaultPreview() {
+    LemonApp()
 }
