76a52d047bd00e5d83b6a551a46ac89a3c573f65
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-04-23 08:26:16 -0700

    woof!

--- app/src/main/java/info/fekri/jetapp/MainActivity.kt
+++ app/src/main/java/info/fekri/jetapp/MainActivity.kt
@@ -1,34 +1,43 @@
 package info.fekri.jetapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import info.fekri.jetapp.ui.theme.JetAppTheme
+import info.fekri.jetapp.data.Dog
+import info.fekri.jetapp.data.dogs
+import info.fekri.jetapp.ui.theme.WoofTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Scaffold
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetAppTheme {
-                LemonApp()
+            WoofTheme(darkTheme = false) {
+                WoofApp()
             }
         }
     }
@@ -36,122 +45,128 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun DefaultPreview() {
-    LemonApp()
+fun LightThemePreview() {
+    WoofTheme(darkTheme = false) {
+        WoofApp()
+    }
 }
 
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun LemonApp() {
-    var currentStep by remember { mutableStateOf(1) }
+fun DarkThemePreview() {
+    WoofTheme(darkTheme = true) {
+        WoofApp()
+    }
+}
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
+
+/**
+ * Composable that displays an app bar and a list of dogs.
+ */
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun WoofApp() {
+    Scaffold(
+        topBar = {
+            WoofTopAppBar()
+        }
     ) {
-        when (currentStep) {
-
-            1 -> {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.select_lemon),
-                        fontFamily = FontFamily.Cursive,
-                        fontSize = 30.sp,
-                        color = Color.Green
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.lemon_tree),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .clickable {
-                                currentStep = 2
-                            }
-                    )
-
-                }
+        LazyColumn(modifier = Modifier.background(MaterialTheme.colors.background)) {
+            items(dogs.size) {
+                DogItem(dog = dogs[it])
             }
-
-            2 -> {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.lemon_squeeze),
-                        fontFamily = FontFamily.Cursive,
-                        fontSize = 30.sp,
-                        color = Color.Green
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.lemon_squeeze),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .clickable {
-                                currentStep = 3
-                            }
-                    )
-                }
-            }
-
-            3 -> {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.tap_drink),
-                        fontFamily = FontFamily.Cursive,
-                        fontSize = 30.sp,
-                        color = Color.Green
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.lemon_drink),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .clickable {
-                                currentStep = 4
-                            }
-                    )
-                }
-            }
-
-            4 -> {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.restart_message),
-                        fontFamily = FontFamily.Cursive,
-                        fontSize = 30.sp,
-                        color = Color.Green
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.lemon_restart),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .clickable {
-                                currentStep = 1
-                            }
-                    )
-                }
-            }
-
         }
     }
 
 }
+
+/**
+ * Composable that displays a list item containing a dog icon and their information.
+ *
+ * @param dog contains the data that populates the list item
+ * @param modifier modifiers to set to this composable
+ */
+@Composable
+fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+
+    Card(
+        modifier = modifier.padding(8.dp),
+        elevation = 4.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            DogIcon(dog.imageResourceId)
+            DogInformation(dog.name, dog.age)
+        }
+    }
+
+}
+
+/**
+ * Composable that displays a photo of a dog.
+ *
+ * @param dogIcon is the resource ID for the image of the dog
+ * @param modifier modifiers to set to this composable
+ */
+@Composable
+fun DogIcon(@DrawableRes dogIcon: Int, modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier
+            .size(64.dp)
+            .padding(8.dp)
+            .clip(RoundedCornerShape(50)),
+        contentScale = ContentScale.Crop,
+
+        painter = painterResource(dogIcon),
+
+        contentDescription = null
+    )
+}
+
+/**
+ * Composable that displays a dog's name and age.
+ *
+ * @param dogName is the resource ID for the string of the dog's name
+ * @param dogAge is the Int that represents the dog's age
+ * @param modifier modifiers to set to this composable
+ */
+@Composable
+fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = stringResource(dogName),
+            modifier = modifier.padding(top = 8.dp),
+            style = MaterialTheme.typography.h2
+        )
+        Text(
+            text = stringResource(R.string.years_old, dogAge),
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .background(color = MaterialTheme.colors.primary)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(64.dp)
+                .padding(8.dp),
+            painter = painterResource(R.drawable.ic_woof_logo),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.h1
+        )
+    }
+}
+
